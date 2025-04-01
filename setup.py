6375c39318d1248d1428d7ddf54cc9c2bf85cd7a
Sohamm31 <sohamm3107@gmail.com>
2025-04-01 23:09:35 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mindly-Mental Health Chatbot',
+    version= '0.0.0',
+    author= 'Soham Khopkar',
+    author_email= 'sohamm3107@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
